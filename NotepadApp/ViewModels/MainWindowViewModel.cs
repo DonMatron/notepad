9fedfe2e534a178cbfad6190a230145e03bbb592
Danylo Prudkyi <endermatrix.mmos@gmail.com>
2023-12-21 19:51:16 +0100

    Implemented Button counter

--- NotepadApp/ViewModels/MainWindowViewModel.cs
+++ NotepadApp/ViewModels/MainWindowViewModel.cs
@@ -2,8 +2,8 @@
 {
     public partial class MainWindowViewModel : ViewModelBase
     {
-#pragma warning disable CA1822 // Mark members as static
-        public string Greeting => "Welcome to Avalonia!";
-#pragma warning restore CA1822 // Mark members as static
+        public static string Greeting => "Welcome to Avalonia!";
+        public static string ButtonAddClicked => "Add " + IncrementValue;
+        public static int IncrementValue => 2;
     }
 }
--- NotepadApp/Views/MainWindow.axaml.cs
+++ NotepadApp/Views/MainWindow.axaml.cs
@@ -1,12 +1,22 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using NotepadApp.ViewModels;
 
 namespace NotepadApp.Views
 {
     public partial class MainWindow : Window
     {
+        private int count = 0;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public void ClickHandler(object sender, RoutedEventArgs args)
+        {
+            count += MainWindowViewModel.IncrementValue;
+            Message.Text = count.ToString();
         }
     }
 }
